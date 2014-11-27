cd24428b8f0a736327703ddd00ce1ec510ebc7ed
TKatra <tobiaskatra@hotmail.com>
2014-11-27 12:08:15 +0100

    Corrected the syntax errors in main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // Vänta tills DOM är redo
-$(function(){ 
-	$('#addBook').submit(addNewBook);	
-	function addNewBook(){
+$(function()
+{
+	$('#addBook').submit(addNewBook);
+	function addNewBook()
+	{
     // Ladda inte om sidan när man trycker på knappen
 	return false;
  
- 	}); 
+ 	}
 });
