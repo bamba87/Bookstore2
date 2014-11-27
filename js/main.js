50c6e48276cc1933c5b8eb05b9df00c4c205afa6
TKatra <tobiaskatra@hotmail.com>
2014-11-27 12:41:39 +0100

    Added the function to contact the database with in main.js.

--- js/main.js
+++ js/main.js
@@ -6,11 +6,26 @@ $(function()
 	{
 		$('#addBook').submit(addNewBook);
 	}
+
+	//Denna funktionen tar emot ett object med infon till den svarta lådan och den tar också emot funktionen som ska köras efteråt.
+	//
+	//En bra sak att veta är att när du lägger in en funktion i inparametern "successFunction" är att du ska INTE skriva "doStuff()" utan "doStuff"
+	//annars kommer den försöka kalla på "doStuff" direkt innan du kallar på "contactDatabase" med andra ord: inte bra.
+	function contactDatabase(request, successFunction)
+	{
+		$.ajax({
+			url:"libs/sql-ajax-json.php",
+			dataType: "json",
+			data: request,
+			success: successFunction
+		});
+	}
 	
 	function addNewBook()
 	{
     // Ladda inte om sidan när man trycker på knappen
 	return false;
 	}
+
 	siteStartup();
  });
