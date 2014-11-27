cddd419584bc7ef01842b7eb68771fafe774bf31
TKatra <tobiaskatra@hotmail.com>
2014-11-27 12:25:38 +0100

    Removed a line break in index.html
    Created the function "siteStartup()" in main.js for the site startup.
    All things that will created and/or changed when the page loads shall be in this function.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 // Vänta tills DOM är redo
 $(function()
 {
-	$('#addBook').submit(addNewBook);
+	//Alla saker som ska köras när sidan laddats ska vara i denna funktionen
+	function siteStartup()
+	{
+		$('#addBook').submit(addNewBook);
+	}
+	
 	function addNewBook()
 	{
     // Ladda inte om sidan när man trycker på knappen
 	return false;
- 
- 	}
-});
+	}
+	siteStartup();
+ });
